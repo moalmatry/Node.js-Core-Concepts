f05a82cfbc3aeb822315a5e9b8eed195a4823b0a
moalmatry <moalmatry2000@gmail.com>
2024-09-30 18:41:01 +0300

    finish chat app

--- chat/client/client.mjs
+++ chat/client/client.mjs
@@ -5,6 +5,23 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = async (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
+let id;
 
 const socket = net.createConnection(
   {
@@ -14,14 +31,34 @@ const socket = net.createConnection(
   async () => {
     console.log("connected to the server");
 
-    const message = await rl.question("enter message => ");
+    const ask = async () => {
+      const message = await rl.question("enter message > ");
+      // move cursor to upper
+      await moveCursor(0, -1);
+      // clear the current line
+      await clearLine(0);
+      socket.write(`${id}-message-${message}`);
+    };
 
-    socket.write(message);
+    ask();
+    socket.on("data", async (data) => {
+      console.log();
+      await moveCursor(0, -1);
+      await clearLine(0);
+      if (data.toString("utf-8").substring(0, 2) === "id") {
+        // when getting id
+        id = data.toString("utf-8").substring(3);
+
+        console.log(`welcome to our server - your id is ${id} !\n`);
+      } else {
+        // when getting message
+
+        console.log(data.toString("utf-8"));
+        await ask();
+      }
+    });
   }
 );
-socket.on("data", (data) => {
-  console.log(data.toString("utf-8"));
-});
 
 // when the connection is ended with the server
 socket.on("end", () => {
--- chat/server/server.mjs
+++ chat/server/server.mjs
@@ -5,13 +5,35 @@ const server = net.createServer();
 // array to hold all connected clients. This is a basic implementation. In a real-world scenario, you'd want to use a more efficient data structure like a Set or Map.
 const clients = [];
 server.on("connection", (socket) => {
-  console.log("new client");
+  console.log("new connection to the server");
+  const clientId = clients.length + 1;
+
+  // broadcast to all connected clients when someone connected
+  clients.map((client) => client.socket.write(`User ${clientId} joined !`));
+
+  socket.write(`id-${clientId}`);
   socket.on("data", (data) => {
-    clients.map((client) => client.write(data));
+    const dataString = data.toString("utf-8");
+    const id = dataString.substring(0, dataString.indexOf("-"));
+    const message = dataString.substring(dataString.indexOf("-message-") + 9);
+    clients.map((client) => client.socket.write(`> User ${id}: ${message}`));
     socket.write(data);
   });
 
-  clients.push(socket);
+  // write messages to every one when some one leaves from chat room
+  socket.on("end", () => {
+    clients.map((client) => client.socket.write(`User ${clientId} left!`));
+  });
+  socket.on("error", () => {
+    clients.map((client) => {
+      client.socket.write(`User ${clientId} left!`);
+    });
+  });
+
+  clients.push({
+    id: String(clientId),
+    socket,
+  });
 });
 
 // start the server in 8000 port
