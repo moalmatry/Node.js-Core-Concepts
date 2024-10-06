2ed3023e88b1fa4fb13f5d5f1a04213ddb0855db
moalmatry <moalmatry2000@gmail.com>
2024-10-06 03:51:17 +0300

    add simple api

--- http/web-server/server.mjs
+++ http/web-server/server.mjs
@@ -15,8 +15,46 @@ server.on("request", async (req, res) => {
     res.setHeader("Content-Type", "text/css");
     const fileHandle = await fs.open("./public/styles.css", "r");
     const fileStream = fileHandle.createReadStream();
+    fileStream.pipe(res);
+  }
+
+  if (req.url === "/script.js" && req.method === "GET") {
+    res.setHeader("Content-Type", "text/javascript");
+    const fileHandle = await fs.open("./public/script.js", "r");
+    const fileStream = fileHandle.createReadStream();
 
     fileStream.pipe(res);
+  }
+  if (req.url === "/login" && req.method === "POST") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    const body = {
+      message: "Logging you in ...",
+    };
+
+    res.end(JSON.stringify(body));
+  }
+  if (req.url === "/user" && req.method === "PUT") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 401;
+    const body = {
+      message: "You First has to login",
+    };
+
+    res.end(JSON.stringify(body));
+  }
+
+  if (req.url === "/upload" && req.method === "PUT") {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+
+    const fileHandle = await fs.open("./storage/image.jpg", "w");
+    const fileStream = fileHandle.createWriteStream();
+
+    req.pipe(fileStream);
+    req.on("end", () => {
+      res.end(JSON.stringify({ message: "file uploaded successfully" }));
+    });
   }
 });
 
