6da041463645f50a00e5be9fa5ebad84f8376b4a
moalmatry <moalmatry2000@gmail.com>
2024-12-14 20:05:43 +0200

    feat: start compression setion

---
+++ compression/app.js
@@ -0,0 +1,24 @@
+const zlib = require("zlib");
+const fs = require("fs");
+
+zlib.createGzip();
+
+const fileName = "text.txt";
+const content = "1";
+const iterations = 100000000;
+
+const writeStream = fs.createWriteStream(fileName);
+
+for (let i = 0; i < iterations; i++) {
+  writeStream.write(content);
+}
+
+writeStream.end();
+
+writeStream.on("finish", () => {
+  console.log("File has been written successfully.");
+});
+
+writeStream.on("error", (err) => {
+  console.error("An error occurred:", err.message);
+});
